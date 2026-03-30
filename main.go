6556dbfe8349e761d3eb8fbfb27c36b41bde7256
Nikita Gorobets <tier1993@gmail.com>
2026-03-30 21:18:44 +0300

    pools added

--- main.go
+++ main.go
@@ -5,10 +5,32 @@ import (
 	"net/http"
 )
 
+var pools = make(map[string]string)
+
 func main() {
 	http.HandleFunc("/pool", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello world")
-		fmt.Println(r)
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			http.Error(w, "code is required", http.StatusBadRequest)
+			return
+		}
+		if r.Method == "POST" {
+			body := make([]byte, r.ContentLength)
+			r.Body.Read(body)
+			pools[code] = string(body)
+			fmt.Fprint(w, "saved")
+			return
+		}
+		if r.Method == "GET" {
+			value, ok := pools[code]
+			if !ok {
+				http.Error(w, "not found", http.StatusNotFound)
+				return
+			}
+			fmt.Fprintln(w, value)
+			return
+		}
+		fmt.Fprint(w, "unsupported method")
 	})
 	fmt.Println("Server started on :8080")
 	http.ListenAndServe(":8080", nil)
