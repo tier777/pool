b95319991243028b0ca0c01682af5ccfe483a126
Nikita Gorobets <tier1993@gmail.com>
2026-04-02 01:37:03 +0300

    autosave with polling

--- web/app.js
+++ web/app.js
@@ -1,6 +1,12 @@
+let timeout = null;
+let isTyping = false;
+
 async function load() {
+  if (isTyping) return;
+
   const res = await fetch("/api/pool");
   const data = await res.json();
+
   document.getElementById("text").value = data.content;
 }
 
@@ -12,15 +18,39 @@ async function save() {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ content }),
   });
+
+  showStatus("Saved");
+}
+
+function debounceSave() {
+  clearTimeout(timeout);
+
+  isTyping = true;
+  showStatus("Typing...");
+
+  timeout = setTimeout(() => {
+    isTyping = false;
+    save();
+  }, 500);
+}
+
+function showStatus(text) {
+  document.getElementById("status").innerText = text;
 }
 
 function copyText() {
   const text = document.getElementById("text");
   text.select();
   document.execCommand("copy");
+  showStatus("Copied");
 }
 
-document.getElementById("save").onclick = save;
+// events
+document.getElementById("text").addEventListener("input", debounceSave);
 document.getElementById("copy").onclick = copyText;
 
+// polling
+setInterval(load, 2000);
+
+// initial load
 load();
